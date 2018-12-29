3f08c82bef786cb5e3343b1514396d0ccaa3788e
Ganderman-Dan <46117179+Ganderman-Dan@users.noreply.github.com>
2018-12-28 19:56:41 -0500

    updated asset folder
    at least i think i did, i have no idea what im doing

---
+++ TeamClassProject/Assets/Scripts/HeathAndShield.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeathAndShield : MonoBehaviour
+{
+    /* please put your name at the end of important comments so that we know who wrote what, so that if one of us has a problem understanding
+     * what the code does, we can directly ask the person, instead of asking everyone
+     * also im putting this block comment at the top of every script, sorry if it gets repetitive
+     * -Ganderman Dan
+     */
+
+    // this script should be where all the health and shield related stuff goes. This also includes functions that take care of taking damage. -Ganderman Dan    
+
+
+
+    [Header("Health and Shield")]
+    public float maxHealth;
+    private float currentHealth;
+    public float maxShield, shieldWaitTime, shieldRegenSpeed; //dont know if we will have shield properties in the game but i put it here just in case -Ganderman Dan
+    private float currentShield;
+    /*
+     * shields, (at least what im thinking, will be a small portion of health that that must fully deplete before the players "health" starts to ...
+     * go down. Shields will also have the ability to regenerate after a period of not getting hit.)
+     * 
+     * shieldWaitTime - is the amount of time you cant take damage for before you start to regenerate your armor
+     * shieldRegenSpeed - is how fast you regen your shield
+     * -Ganderman Dan
+     */
+    [HideInInspector]
+    public bool isInvinsible;
+    private float isInvinsibleTimer;
+
+    void Start()
+    {
+        
+    }
+
+    //i dont think we will need to use FixedUpdate() in this script, but i put it here just in case, and so we dont forget about it
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ TeamClassProject/Assets/Scripts/Movement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    /* please put your name at the end of important comments so that we know who wrote what, so that if one of us has a problem understanding
+     * what the code does, we can directly ask the person, instead of asking everyone
+     * also im putting this block comment at the top of every script, sorry if it gets repetitive
+     * -Ganderman Dan
+     */
+
+    /* this script should be where all the movement related stuff should go. 
+     * I think we should also put knockback related stuff here since that has to do with movement. 
+     *            (ex. angle you fly when you get hit, and how far you fly back when you get hit, etc.)
+     * But if you'd rather have a seperate script for that, let me know  -Ganderman Dan 
+     */
+
+    [Header("Movement Modifiers")]
+    public float gravity;
+    public float walkSpeed, airSpeed, runSpeed; //I dont know if running is in the game, but i put the variable there just in case -Ganderman Dan 
+    public float numJumps, jumpForce;
+    public float weight;  
+    public bool canWallCling;
+    [HideInInspector]
+    public float hitStun; 
+
+
+    void Start()
+    {
+        
+    }
+
+   /* if you're doing something with the rigidbody and movement, do it here, it's better
+    * check here for reference https://docs.unity3d.com/ScriptReference/MonoBehaviour.FixedUpdate.html
+    * more about the differnces between the two update functions https://unity3d.com/learn/tutorials/topics/scripting/update-and-fixedupdate
+    * -Ganderman Dan
+    */
+    void FixedUpdate()
+    {
+        
+    }
+
+    //anything that doesn't have to do with rigidbody movement, do here
+    void Update()
+    {
+
+    }
+
+
+    /* This function gets called when an enemy hits you
+     * What the arguments are for:
+     *      attackDamage- is the how much the players health/armor goes down.
+     *      attackAngle- is the angle you get sent flying when you get hit. [*possibly* affected by player weight]
+     *      attackForce- is how far back you get sent flying. [affected by player weight]
+     *      hitStun- is how long the player has to wait before they can do anything
+     *      -Ganderman Dan
+     */ 
+    public void GetHit(float attackDamage, float attackAngle, float attackForce, float hitStun)//im probably missing a few arguments
+    {
+        
+    }
+}
