c33929a810d862bbde94d1dc2583835446f79e41
Buscems <46265895+Buscems@users.noreply.github.com>
2019-02-01 15:31:48 -0500

    Still working on Claire/Basic Character Movement

--- TeamClassProject/Assets/Scripts/Claire.cs
+++ TeamClassProject/Assets/Scripts/Claire.cs
@@ -6,8 +6,21 @@ public class Claire : MonoBehaviour
 {
 
     public float maxHealth;
-    public float speed;
+    public int speed;
     public float weight;
+
+    PlatformPlayer player;
+
+    private void Awake()
+    {
+        if(this.GetComponent<PlatformPlayer>() != null)
+        {
+            player = this.GetComponent<PlatformPlayer>();
+            player.maxHealth = maxHealth;
+            player.speed = speed;
+            player.weight = weight;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
