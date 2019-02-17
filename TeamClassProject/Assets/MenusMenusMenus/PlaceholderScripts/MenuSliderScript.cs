864b38013ea39d2a3ba5c2447c573ab9069765b1
NolanSadowski <47158785+NolanSadowski@users.noreply.github.com>
2019-02-16 21:29:54 -0500

    Hope this works fam
    I did what I did before, but better! and hopefully this time Nick doesnt delete my hard work. (I did a basic menu slider when you hit enter and escape)

---
+++ TeamClassProject/Assets/MenusMenusMenus/PlaceholderScripts/MenuSliderScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSliderScript : MonoBehaviour
+{
+
+    private Animator anim;
+    public int sliderInt;
+    public AudioSource menuUp;
+    public AudioSource menuDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        sliderInt = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && sliderInt != 1)
+        {
+            anim.SetInteger("SliderValue", 1);
+            menuUp.Play();
+            sliderInt = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && sliderInt != 2)
+        {
+            anim.SetInteger("SliderValue", 2);
+            menuDown.Play();
+            sliderInt = 2;
+        }
+    }
+}
