c80394ff6f78c43be792de2e5fc18d1fb0344a18
NolanSadowski <47158785+NolanSadowski@users.noreply.github.com>
2019-02-18 13:17:39 -0500

    More Juice
    Added a little screen flash and a video player to the bg

--- TeamClassProject/Assets/MenusMenusMenus/PlaceholderScripts/ButtonScript.cs
+++ TeamClassProject/Assets/MenusMenusMenus/PlaceholderScripts/ButtonScript.cs
@@ -5,27 +5,56 @@ using UnityEngine;
 public class ButtonScript : MonoBehaviour
 {
     public int buttonID;
+    public static int lastButtonOn;
     public GameObject selector;
+    public GameObject flash;
+
+    public GameObject playVideo;
+    public GameObject fightersVideo;
+    public GameObject optionsVideo;
+    public GameObject extrasVideo;
+    public GameObject quitVideo;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastButtonOn = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(lastButtonOn == 1)
+        {
+            playVideo.SetActive(true);
+        } else
+        {
+            playVideo.SetActive(false);
+        }
+
+        if (lastButtonOn == 2)
+        {
+            playVideo.SetActive(true);
+        }
+        else
+        {
+            playVideo.SetActive(false);
+        }
+
+        //Debug.Log(lastButtonOn);
     }
 
     void OnMouseEnter()
     {
         selector.SetActive(true);
+        flash.SetActive(true);
+
+        lastButtonOn = buttonID;
     }
 
     void OnMouseExit()
     {
         selector.SetActive(false);
+        flash.SetActive(false);
     }
 }
