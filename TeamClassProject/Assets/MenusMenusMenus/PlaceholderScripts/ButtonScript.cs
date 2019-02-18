cf66e1ef1cb938e21aa3b18758c2175d2f209455
NolanSadowski <47158785+NolanSadowski@users.noreply.github.com>
2019-02-18 01:22:50 -0500

    Selectors for Menu Buttons
    I made selectors over the 5 different menu buttons with some placeholder audio attached. I think it looks pretty sweet

---
+++ TeamClassProject/Assets/MenusMenusMenus/PlaceholderScripts/ButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public int buttonID;
+    public GameObject selector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        selector.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        selector.SetActive(false);
+    }
+}
