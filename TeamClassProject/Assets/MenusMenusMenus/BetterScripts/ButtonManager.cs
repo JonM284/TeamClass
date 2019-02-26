43872da59aac10d1313e0cda3c4698a0dc052d50
Nolan Sadowski <nolanryansadowskI@outlook.com>
2019-02-26 13:11:44 -0500

    Made some buttons work
    Made the play and quit button work and set up code for the other ones to eventually work

---
+++ TeamClassProject/Assets/MenusMenusMenus/BetterScripts/ButtonManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playBtn()
+    {
+        SceneManager.LoadScene("MixedTogetherScene");
+    }
+
+    public void fighterBtn()
+    {
+        //SceneManager.LoadScene("");
+    }
+
+    public void optionsBtn()
+    {
+        //SceneManager.LoadScene("");
+    }
+
+    public void extrasBtn()
+    {
+        //SceneManager.LoadScene("");
+    }
+
+    public void quitBtn()
+    {
+        Application.Quit();
+    }
+
+    public void backBtn()
+    {
+        //SceneManager.LoadScene("");
+    }
+}
