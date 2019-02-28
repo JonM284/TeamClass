4595e3c2068512f2c435db95fb4f83858721deb5
Justin DiLeonardo <jdsport04@yahoo.com>
2019-02-28 15:33:59 -0500

    Added Colliders with Blow and Vacuum to Machine 5 (SHIP)

---
+++ TeamClassProject/Assets/Scripts/MachinePlatform_Vacuum.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachinePlatform_Vacuum : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<BasicPlayerScript>().gravityUp = 49f;
+            other.gameObject.GetComponent<BasicPlayerScript>().gravityDown = 34f;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<BasicPlayerScript>().gravityUp = 70f;
+            other.gameObject.GetComponent<BasicPlayerScript>().gravityDown = 48f;
+        }
+    }
+
+}
