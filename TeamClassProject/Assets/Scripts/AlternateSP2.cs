9ae41d4133cf611ecb8cc322cd66c306e7cffe92
Justin DiLeonardo <justindileonardo@gmail.com>
2019-03-06 14:41:24 -0500

    Initial setup for Gnome Level +First Machine (Fairy)

---
+++ TeamClassProject/Assets/Scripts/AlternateSP2.cs
@@ -0,0 +1,283 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using Rewired.ControllerExtensions;
+
+public class AlternateSP2 : MonoBehaviour
+{
+
+    //public variables
+    [Tooltip("Speed of the player")]
+    public float speed;
+    [Header("ID numbers")]
+    [Tooltip("Insert player ID numbers")]
+    public int playerNum, teamID;
+
+
+    //private variables
+
+    //This is for the direction of the player
+    //I am aware that it is not actually facing right at the beginning
+    private bool is_Looking_Right = false;
+    private Vector3 original_Scale;
+
+    //This allows us to change what they can do, when they are either at or away from a machine.
+    private enum Status { Free, AtMachine };
+    private Rigidbody2D rb;
+    private Vector2 vel;
+    private float horizontalInput, verticalInput;
+    private Player myPlayer;
+    [SerializeField]
+    private bool is_In_Area = false;
+    //The current status of the player: Free (Away from machine- Free to move)
+    // AtMachine(Player is at machine- not free to move).
+    Status status;
+    //reference to whichever machine the player is going to use
+    private GameObject myMachine;
+
+    //Beyond this point is for animations
+    private Animator anim;
+    
+
+
+    //--------------------------------------------------------------------------------------------------
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        original_Scale = transform.localScale;
+    }
+
+
+
+    //--------------------------------------------------------------------------------------------------
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMachine = null;
+        status = Status.Free;
+        myPlayer = ReInput.players.GetPlayer(playerNum - 1);
+        ReInput.ControllerConnectedEvent += OnControllerConnected;
+        CheckController(myPlayer);
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    // Update is called once per frame
+    void Update()
+    {
+        if (status == Status.Free) {
+            horizontalInput = myPlayer.GetAxisRaw("Horizontal");
+
+            if (horizontalInput >= 0.1f && !is_Looking_Right)
+            {
+                gameObject.transform.localScale = new Vector3(-original_Scale.x, transform.localScale.y, transform.localScale.z);
+                is_Looking_Right = true;
+            }
+            if (horizontalInput <= -0.1f && is_Looking_Right)
+            {
+                gameObject.transform.localScale = new Vector3(original_Scale.x, transform.localScale.y, transform.localScale.z);
+                is_Looking_Right = false;
+            }
+
+        }else
+        {
+            horizontalInput = 0;
+            if (transform.position.x != myMachine.transform.position.x)
+            {
+                transform.position = new Vector3(myMachine.transform.position.x, transform.position.y, transform.position.z);
+            }
+        }
+
+        // If player is infront of machine and they press "Jump" and they are free, set their status to at a machine
+        // Give the machine my inputs.
+        // If the player presses "Jump" and is using a machine, set them to free again.
+        // Also if they press heavy attack, they jump off the machine without using it.
+        if (is_In_Area && (myPlayer.GetButtonDown("Jump")|| myPlayer.GetButtonDown("BasicAttack")))
+        {
+            if (status == Status.Free)
+            {
+                status = Status.AtMachine;
+                if (!myMachine.GetComponent<MachineBehaviour2>().is_In_Use) {
+                    myMachine.GetComponent<MachineBehaviour2>().Commence_Control(playerNum, teamID);
+                }
+                Debug.Log(status);
+            }else if (status == Status.AtMachine)
+            {
+                status = Status.Free;
+                if (myMachine.GetComponent<MachineBehaviour2>().is_In_Use)
+                {
+                    myMachine.GetComponent<MachineBehaviour2>().End_Control();
+                    Debug.Log("Has detached from machine with Jump");
+                }
+                
+                Debug.Log(status);
+            }
+        }else if (is_In_Area && myPlayer.GetButtonDown("HeavyAttack"))
+        {
+            if (myMachine.GetComponent<MachineBehaviour2>().is_In_Use)
+            {
+                myMachine.GetComponent<MachineBehaviour2>().End_Control();
+                myMachine = null;
+                Debug.Log("Has detached from machine with Heavy Attack");
+            }
+            if (status == Status.AtMachine)
+            {
+                status = Status.Free;
+                Debug.Log(status);
+            }
+
+        }
+
+
+        Handle_Animations();
+
+
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    private void FixedUpdate()
+    {
+        if (horizontalInput < -0.1f || horizontalInput > 0.1f)
+        {
+            Movement();
+        }
+    }
+
+
+    void Handle_Animations()
+    {
+        if (status == Status.AtMachine)
+        {
+            anim.SetBool("Is_At_Machine", true);
+        }
+        else
+        {
+            anim.SetBool("Is_At_Machine", false);
+        }
+
+        if (myPlayer.GetAxisRaw("Horizontal") == 0 && status == Status.Free)
+        {
+            anim.SetInteger("Anim_Num", 0);
+        }
+
+        if (Mathf.Abs(myPlayer.GetAxisRaw("Horizontal")) >= 0.1f && status == Status.Free)
+        {
+            anim.SetInteger("Anim_Num", 1);
+        }
+
+        if (status == Status.AtMachine && Mathf.Abs(myPlayer.GetAxisRaw("Horizontal")) == 0 && Mathf.Abs(myPlayer.GetAxisRaw("Vertical")) == 0)
+        {
+            anim.SetInteger("Anim_Num", 2);
+        }
+
+        if (status == Status.AtMachine && (myPlayer.GetAxisRaw("Horizontal") >= 0.1f || myPlayer.GetAxisRaw("Vertical") >= 0.1f))
+        {
+            anim.SetInteger("Anim_Num", 7);
+        }
+
+        if (status == Status.AtMachine && (myPlayer.GetAxisRaw("Horizontal") <= -0.1f || myPlayer.GetAxisRaw("Vertical") <= -0.1f))
+        {
+            anim.SetInteger("Anim_Num", 6);
+        }
+
+
+
+
+
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    //this is a temporary function that is to test whether or not the enum is working correctly.
+    public void SetFree()
+    {
+        status = Status.Free;
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    //This controls the movement of the player.
+    void Movement()
+    {
+        vel.x = horizontalInput * speed;
+        vel.y = 0;
+
+        rb.MovePosition(rb.position + Vector2.ClampMagnitude(vel, speed) * Time.deltaTime);
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Machine")
+        {
+            is_In_Area = true;
+            myMachine = other.gameObject;
+        }
+        
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Machine")
+        {           
+            myMachine = other.gameObject;
+        }
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Machine")
+        {
+            is_In_Area = false;
+            myMachine = null;
+        }
+    }
+
+
+
+    //--------------------------------------------------------------------------------------------------
+    ///rewired functions
+    // Event that triggers when a controller is plugged in to the computer.
+    void OnControllerConnected(ControllerStatusChangedEventArgs arg)
+    {
+        CheckController(myPlayer);
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    //Checks to see if this controller is a PS4 controller. If it is, change the color on the back of the controller.
+    void CheckController(Player player)
+    {
+        foreach (Joystick joyStick in player.controllers.Joysticks)
+        {
+            var ds4 = joyStick.GetExtension<DualShock4Extension>();
+            if (ds4 == null) continue;//skip this if not DualShock4
+            switch (playerNum)
+            {
+                case 4:
+                    ds4.SetLightColor(Color.yellow);
+                    break;
+                case 3:
+                    ds4.SetLightColor(Color.green);
+                    break;
+                case 2:
+                    ds4.SetLightColor(Color.blue);
+                    break;
+                case 1:
+                    ds4.SetLightColor(Color.red);
+                    break;
+                default:
+                    ds4.SetLightColor(Color.white);
+                    Debug.LogError("Player Num is 0, please change to a number >0");
+                    break;
+            }
+
+
+        }
+    }
+}
---
+++ TeamClassProject/Assets/Scripts/MachineBehaviour2.cs
@@ -0,0 +1,235 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using Rewired.ControllerExtensions;
+
+public class MachineBehaviour2 : MonoBehaviour
+{
+    [Tooltip("Is the current machine in use?")]
+    public bool is_In_Use = false;
+    [Header("All Hazards")]
+    [Tooltip("Insert all GameObjects that will be controlled by THIS machine")]
+    public GameObject[] Controlled_Hazard;
+    [Tooltip("Vertical and Horizontal move speed of certain hazzards")]
+    public float speed;
+    [Header("Max Hazzards")]
+    [Tooltip("How many hazzards will this machine have access to?")]
+    public int max_Machines_Amnt;
+    [Tooltip("Max distance for side hazards, max angle for side cannons")]
+    public float Max_range;
+    //These are the different MACHINE TYPES
+    public enum MachineID { Fairy };
+    [Header("Machine Type")]
+    [Tooltip("What type of machine will this be?")]
+    public MachineID mach;
+
+
+    //inputs are for movement
+    private float horizontalInput, verticalInput;
+    //this allows us to change between hazzards
+    private int Current_Haz_Num = 0;
+    //velocity
+    private Vector2 vel;
+
+    public bool can_Use = false;
+    private bool other_can_Use = false;
+    //rewired after this point
+    //myPlayer will properly connect this players inputs to go to the correct location in rewired
+    private Player myPlayer;
+    //This is in order to "Spawn in" objects
+    private ObjectSpawner objectPool;
+    private Vector3 Move_Rotation, originalRotation;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalRotation = new Vector3(Controlled_Hazard[Current_Haz_Num].transform.rotation.x,
+            Controlled_Hazard[Current_Haz_Num].transform.rotation.y,
+            Controlled_Hazard[Current_Haz_Num].transform.rotation.z);
+
+        Move_Rotation = new Vector3(0, 0, 90);
+        //get an instance of the object spawner so we can spawn objects
+        objectPool = ObjectSpawner.Instance;
+
+
+        //only do this if this machine is of type "Background Cannon" 
+        if (mach == MachineID.Fairy)
+        {
+            Controlled_Hazard[0].SetActive(false);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the machine is in use
+        if (is_In_Use) {
+
+            if (mach == MachineID.Fairy)
+            {
+                verticalInput = myPlayer.GetAxisRaw("Vertical");
+                horizontalInput = myPlayer.GetAxisRaw("Horizontal");
+                FairyControl();
+            }
+            /*else if (mach == MachineID.BackgroundCannon)
+            {
+                verticalInput = myPlayer.GetAxisRaw("Vertical");
+                horizontalInput = myPlayer.GetAxisRaw("Horizontal");
+                BackgroundCannonMovement();
+            }
+            else if (mach == MachineID.SideHazard)
+            {
+                verticalInput = myPlayer.GetAxisRaw("Vertical");
+                SideHazzardControl();
+            }
+            else if (mach == MachineID.SpecialPlatform)
+            {
+                verticalInput = myPlayer.GetAxisRaw("Vertical");
+                SpecialPlatformBehaviour();
+            }
+            else if (mach == MachineID.MiddlePlatform)
+            {
+
+                MiddlePlatformBehavior();
+            }*/
+        }
+
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Function description: This contains all movement and interactions for the Side Cannons
+    /// </summary>
+    void FairyControl()
+    {
+
+        vel.x = horizontalInput * speed;
+        vel.y = verticalInput * speed;
+
+        print(vel.y);
+
+        //this allows the player to move the crosshair
+        Controlled_Hazard[Current_Haz_Num].GetComponent<Rigidbody2D>().MovePosition(Controlled_Hazard[Current_Haz_Num].GetComponent<Rigidbody2D>().position
+            + Vector2.ClampMagnitude(vel, speed) * Time.deltaTime);
+
+     
+
+    }
+
+
+
+    //--------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Function Description: This contains all movement and interactions for the Background Cannon.
+    /// </summary>
+    void BackgroundCannonMovement()
+    {
+        vel.x = horizontalInput * speed;
+        vel.y = verticalInput * speed;
+
+        //this allows the player to spawn an object in the position where the crosshair is
+        if (myPlayer.GetButtonDown("Jump") && can_Use)
+        {
+            objectPool.SpawnFromPool("Tester", Controlled_Hazard[Current_Haz_Num].transform.position, Quaternion.identity);
+            End_Control();
+            Debug.Log("Has Spawned object");
+        }
+
+        //this allows the player to move the crosshair
+        Controlled_Hazard[Current_Haz_Num].GetComponent<Rigidbody2D>().MovePosition(Controlled_Hazard[Current_Haz_Num].GetComponent<Rigidbody2D>().position
+            + Vector2.ClampMagnitude(vel, speed) * Time.deltaTime);
+    }
+
+
+
+    
+
+    //--------------------------------------------------------------------------------------------------
+    /// <summary>
+    ///
+    //This Function requires access to the correct player number in order to have the correct player interact with the hazzards.
+    /// </summary>
+    /// <param name="playerNum">Player ID from the support character that is activating this machine.</param>
+    /// <param name="teamID">Player's Team ID</param>
+    public void Commence_Control(int playerNum, int teamID)
+    {
+        // Recieve the number of a player and use it as my inputs.
+        myPlayer = ReInput.players.GetPlayer(playerNum - 1);
+
+        is_In_Use = true;
+
+        StartCoroutine(waitForUse());
+
+        if (mach == MachineID.Fairy)
+        {
+            Controlled_Hazard[0].SetActive(true);
+        }
+
+            Debug.Log("Player:"+playerNum+ " has activated hazzard: "+mach);
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Turns off the inputs that were being recieved from the player.
+    /// Player is no longer going to use this machine for now.
+    /// </summary>
+    public void End_Control()
+    {
+        is_In_Use = false;
+        can_Use = false;
+        other_can_Use = false;
+        // The playerID "-1" does not exist, therefore, the inputs will never be recieved.
+        myPlayer = ReInput.players.GetPlayer(-1);
+
+        if (mach == MachineID.Fairy)
+        {
+            Controlled_Hazard[0].SetActive(false);
+        }
+
+        Debug.Log("Player has deactivated machine: "+transform.name);
+    }
+
+
+
+
+    //--------------------------------------------------------------------------------------------------
+    // This function just draws Gizmos in unity
+    /*private void OnDrawGizmos()
+    {
+        if (mach == MachineID.SideHazard) {
+            Gizmos.color = new Color32(255, 0, 0, 200);
+            for (int i = 0; i < Controlled_Hazard.Length; i++) {
+                Gizmos.DrawLine(new Vector3(Controlled_Hazard[i].transform.position.x,
+                    Controlled_Hazard[i].transform.position.y + 0.6f, Controlled_Hazard[i].transform.position.x),
+                    new Vector3(Controlled_Hazard[i].transform.position.x,
+                    Controlled_Hazard[i].transform.position.y - 0.6f, Controlled_Hazard[i].transform.position.x));
+            }
+        }
+
+        if (mach == MachineID.BackgroundCannon)
+        {
+            Gizmos.color = new Color32(0,255,0, 80);
+        }
+        if (mach == MachineID.SideCannon)
+        {
+            Gizmos.color = new Color32(0, 0, 255, 125);
+        }
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+    }
+    */
+
+    IEnumerator waitForUse()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Debug.Log("Now can use");
+        can_Use = true;
+        other_can_Use = true;
+    }
+
+}
