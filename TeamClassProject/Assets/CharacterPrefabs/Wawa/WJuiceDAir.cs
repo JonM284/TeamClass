399198970f8a767b5ff522e0b9707552184e8977
nickronk <nickronk123@gmail.com>
2019-03-31 20:42:28 -0400

    Watermelon/Seed Addition (to an extent)
    still need to get melon juice/damage

---
+++ TeamClassProject/Assets/CharacterPrefabs/Wawa/WJuiceDAir.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WJuiceDAir : MonoBehaviour
+{
+    public GameObject floorjuice;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            try
+            {
+                Debug.Log("hi1");
+                GameObject melonDownJ = Instantiate(floorjuice, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ TeamClassProject/Assets/CharacterPrefabs/Wawa/WJuiceFloor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WJuiceFloor : MonoBehaviour
+{
+    void Update()
+    {
+        //add destroy feature here
+        //juice needs to be removed
+    }
+}
