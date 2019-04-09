2fad60222dd9c9fb9e26264465bd5a493255b402
nickronk <nickronk123@gmail.com>
2019-04-09 15:17:19 -0400

    Menu Sound (slider) Addition

--- TeamClassProject/Assets/pauserScript.cs
+++ TeamClassProject/Assets/pauserScript.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class pauserScript : MonoBehaviour
 {
 
-     public Canvas pPanel;
-     public Canvas canv;
+    public Canvas pPanel;
+    public Canvas canv;
+
+    public Slider sfxS, musS;
+
+    private Vector2 originsfS, originmuS;
+
+
+
 
      void Start()
      {
+        originmuS = musS.transform.position;
+        originsfS = sfxS.transform.position;
+
+
+        sfxS.transform.position = new Vector2(9999, 9999);
+        musS.transform.position = new Vector2(9999, 9999);
+
         pPanel.enabled = false;
+        sfxS.enabled = false;
+        musS.enabled = false;
+
      }
 
 
@@ -19,30 +38,58 @@ public class pauserScript : MonoBehaviour
 
          print("1GoGo");
          if (Input.GetKeyDown(KeyCode.Escape))
+         {
+             if (pPanel.enabled)
              {
-                 if (pPanel.enabled)
-                 {
-                     Continuer();
-                 }
-                 else
-                 {
-                     Pauser();
-                 }
+                 Continuer();
              }
+             else
+             {
+                 Pauser();
+             }
+         }
      }
-    public void Pauser()
-     {
-         Time.timeScale = 0;
-        pPanel.enabled = true;
-        print("Pause!");
-        canv.enabled = false;
-     }
-     public void Continuer()
-     {
-         Time.timeScale = 1;
-        pPanel.enabled = false;
-        print("Continue!");
-        canv.enabled = true;
+
+
+   public void Pauser()
+   {
+      Time.timeScale = 0;
+      pPanel.enabled = true;
+      print("Pause!");
+      canv.enabled = false;
+   }
+
+   public void Continuer()
+   {
+      Time.timeScale = 1;
+      pPanel.enabled = false;
+      print("Continue!");
+      canv.enabled = true;
+
+        sfxS.enabled = false;
+        musS.enabled = false;
+        sfxS.transform.position = new Vector2(9999, 9999);
+        musS.transform.position = new Vector2(9999, 9999);
+
+    }
+
+    public void ScriptFSound()
+    {
+        if (sfxS.enabled==true)
+        {
+            sfxS.enabled = false;
+            musS.enabled = false;
+            sfxS.transform.position = new Vector2(9999, 9999);
+            musS.transform.position = new Vector2(9999, 9999);
+
+        }
+        else
+        {
+            sfxS.transform.position = originsfS;
+            musS.transform.position = originmuS;
+            sfxS.enabled = true;
+            musS.enabled = true;
+        }
     }
 
 }
