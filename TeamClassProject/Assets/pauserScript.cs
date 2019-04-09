9dbd0b4bdc6d6ee409e9481bb8309d54c8fde5ea
nickronk <nickronk123@gmail.com>
2019-04-08 22:54:33 -0400

    Menu made. Now to make the buttons...do things. Resume works!

---
+++ TeamClassProject/Assets/pauserScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauserScript : MonoBehaviour
+{
+
+     public Canvas pPanel;
+     public Canvas canv;
+
+     void Start()
+     {
+        pPanel.enabled = false;
+     }
+
+
+     void Update()
+     {
+
+         print("1GoGo");
+         if (Input.GetKeyDown(KeyCode.Escape))
+             {
+                 if (pPanel.enabled)
+                 {
+                     Continuer();
+                 }
+                 else
+                 {
+                     Pauser();
+                 }
+             }
+     }
+    public void Pauser()
+     {
+         Time.timeScale = 0;
+        pPanel.enabled = true;
+        print("Pause!");
+        canv.enabled = false;
+     }
+     public void Continuer()
+     {
+         Time.timeScale = 1;
+        pPanel.enabled = false;
+        print("Continue!");
+        canv.enabled = true;
+    }
+
+}
+
