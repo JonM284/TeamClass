1a91cbac495463212ac747fe0ff160c79a291750
Buscems <46265895+Buscems@users.noreply.github.com>
2019-04-16 15:12:59 -0400

    Redoing Claire's ult

---
+++ TeamClassProject/Assets/Scripts/ClaireUlt.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClaireUlt : MonoBehaviour
+{
+
+    public float duration;
+    public float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DestroyUlt()
+    {
+        yield return new WaitForSeconds(duration);
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+
+        }
+    }
+
+}
