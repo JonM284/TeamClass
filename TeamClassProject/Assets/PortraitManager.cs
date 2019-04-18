1baf835424ac91002f83677ee391083627f24f68
Ganderman-Dan <46117179+Ganderman-Dan@users.noreply.github.com>
2019-04-18 16:50:22 -0400

    Character portraits now show when you win

---
+++ TeamClassProject/Assets/PortraitManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PortraitManager : MonoBehaviour
+{
+    bool team1Won;
+
+    [Header("Teammate 1")]
+    public Image claireLeft;
+    public Image gilbertLeft;
+    [Header("Teammate 2")]
+    public Image claireRight;
+    public Image gilbertRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        claireLeft.enabled = false;
+        gilbertLeft.enabled = false;
+        claireRight.enabled = false;
+        gilbertRight.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (team1Won)
+        {
+            //player 1
+            if (PlayerPrefs.GetInt("Player1Character") == 1)
+            {
+                claireLeft.enabled = true;
+            }else if(PlayerPrefs.GetInt("Player1Character") == 2)
+            {
+                gilbertLeft.enabled = true;
+            }
+
+            //player 2
+            if (PlayerPrefs.GetInt("Player2Character") == 1)
+            {
+                claireRight.enabled = true;
+            }
+            else if (PlayerPrefs.GetInt("Player2Character") == 2)
+            {
+                gilbertRight.enabled = true;
+            }
+        }
+        else
+        {
+            //player 3
+            if (PlayerPrefs.GetInt("Player3Character") == 1)
+            {
+                claireLeft.enabled = true;
+            }
+            else if (PlayerPrefs.GetInt("Player3Character") == 2)
+            {
+                gilbertLeft.enabled = true;
+            }
+
+            //player 4
+            if (PlayerPrefs.GetInt("Player4Character") == 1)
+            {
+                claireRight.enabled = true;
+            }
+            else if (PlayerPrefs.GetInt("Player4Character") == 2)
+            {
+                gilbertRight.enabled = true;
+            }
+        }
+    }
+}
