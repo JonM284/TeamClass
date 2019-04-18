75056b3d2779c5ccd242035ec63a30c5af0349bb
JonM284 <44879704+JonM284@users.noreply.github.com>
2019-04-18 03:36:01 -0400

    Support UI
    -support UI moves with screen shake and not with camera

---
+++ TeamClassProject/Assets/TESTING_ASSETS/Temp_Scripts/UI_Follower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Follower : MonoBehaviour
+{
+    public Camera cam;
+    public Transform target;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = cam.WorldToScreenPoint(new Vector3(target.position.x + offset.x,
+                target.position.y + offset.y, target.position.z + offset.z));
+    }
+}
