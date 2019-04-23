4e6813293c608a18e280bffaff7f90dd0786253f
nickronk <nickronk123@gmail.com>
2019-04-23 15:06:47 -0400

    winScreen delay adding

--- TeamClassProject/Assets/simpleBScript.cs
+++ TeamClassProject/Assets/simpleBScript.cs
@@ -9,37 +9,34 @@ using Rewired.ControllerExtensions;
 
 public class simpleBScript : MonoBehaviour
 {
+    private int countDown;
     public EventSystem es;
     public GameObject playButton;
 
     // Start is called before the first frame update
     void Start()
     {
+        countDown = 250;
         es.SetSelectedGameObject(null);
     }
 
     // Update is called once per frame
     void Update()
     {
+        countDown--;
+
         if (es.currentSelectedGameObject == null)
         {
             es.SetSelectedGameObject(es.firstSelectedGameObject);
         }
-
-        /*Player myPlayer = ReInput.players.Players[0];
-
-
-        if (myPlayer.GetButtonDown("Jump"))
-        {
-            SceneManager.LoadScene("NolanScene");
-        }
-        */
-
     }
 
     public void Go_To_Main_Menu()
     {
-        SceneManager.LoadScene("NolanScene");
+        if (countDown < 0)
+        {
+            SceneManager.LoadScene("NolanScene");
+        }
     }
 
 
