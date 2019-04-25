c46eca655e84fdfe7e6f683cd9b70c49c8123cb3
nickronk <nickronk123@gmail.com>
2019-04-25 14:42:53 -0400

    option scene start make
    nolan lied

---
+++ TeamClassProject/Assets/optionScene.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using Rewired;
+using Rewired.ControllerExtensions;
+
+public class optionScene : MonoBehaviour
+{
+
+    public Slider sfxS, musS;
+    public AudioMixerGroup fx, mc;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    public void AudioChanging()
+    {
+        //print(sfxS.value);
+        fx.audioMixer.SetFloat("VolumeSF", sfxS.value);
+        mc.audioMixer.SetFloat("VolumeMu", musS.value);
+    }
+
+    public void quitGame()
+    {
+        SceneManager.LoadScene("NolanScene");
+    }
+
+}
