60715778ea0dceac9f6d5f1a3bad1c1483f098b0
Buscems <46265895+Buscems@users.noreply.github.com>
2019-04-29 13:22:47 -0400

    I don't know what these are but sure

--- TeamClassProject/Assets/BuscemiFixEverything/BasePlayer.cs
+++ TeamClassProject/Assets/BuscemiFixEverything/BasePlayer.cs
@@ -284,7 +284,7 @@ public class BasePlayer : MonoBehaviour
                 {
                     anim.SetFloat("Attack", 0);
                 }
-                if(Mathf.Abs())
+                //if(Mathf.Abs())
 
                 anim.ResetTrigger("Basic");
                 anim.SetTrigger("Basic");
