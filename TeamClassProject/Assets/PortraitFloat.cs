1db51b606e75eb04ff3e25adac7288e495753a8e
Nolan Sadowski <nolanryansadowski@outlook.com>
2019-04-30 13:51:26 -0400

    Fighter Scene Working
    YEah

---
+++ TeamClassProject/Assets/PortraitFloat.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Rewired;
+using Rewired.ControllerExtensions;
+using UnityEngine.SceneManagement;
+
+public class PortraitFloat : MonoBehaviour
+{
+    float originalY;
+    public int selection;
+    public Image clairePic;
+    public Image gilbertPic;
+    public TextMeshProUGUI claireText;
+    public TextMeshProUGUI gilbertText;
+    public TextMeshProUGUI claireText2;
+    public TextMeshProUGUI gilbertText2;
+
+    public float floatStrength;
+
+    public float timer;
+    public float timerMax;
+
+    void Start()
+    {
+        this.originalY = this.transform.position.y;
+        selection = 1;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,
+        originalY + ((float)Mathf.Sin(Time.time * 2) * floatStrength),
+        transform.position.z);
+
+        timer++;
+
+        if (timer >= timerMax)
+        {
+            timer = timerMax;
+        }
+
+        //Iterating through Players (excluding the System Player)
+        for (int i = 0; i < ReInput.players.playerCount; i++)
+        {
+            Player myPlayer = ReInput.players.Players[i];
+
+            if (myPlayer.GetAxis("Horizontal") <= -0.1f && timer >= timerMax)
+            {
+                timer = 0;
+                selection--;
+            }
+
+            if (myPlayer.GetAxis("Horizontal") >= 0.1f && timer >= timerMax)
+            {
+                timer = 0;
+                selection++;
+            }
+
+            if (selection <= -1)
+            {
+                selection = 1;
+            }
+
+            if (selection >= 2)
+            {
+                selection = 0;
+            }
+
+            if (selection == 1)
+            {
+                clairePic.enabled = true;
+                claireText.enabled = true;
+                claireText2.enabled = true;
+                gilbertPic.enabled = false;
+                gilbertText.enabled = false;
+                gilbertText2.enabled = false;
+            }
+            else
+            {
+                gilbertPic.enabled = true;
+                gilbertText.enabled = true;
+                gilbertText2.enabled = true;
+                clairePic.enabled = false;
+                claireText.enabled = false;
+                claireText2.enabled = false;
+            }
+
+            if (myPlayer.GetButtonDown("HeavyAttack"))
+            {
+                //Go back to menu
+                SceneManager.LoadScene("NolanScene");
+            }
+
+        }
+    }
+}
---
+++ TeamClassProject/Assets/RotateThis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateThis : MonoBehaviour
+{
+    public float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //z += 90;
+        transform.rotation = Quaternion.Euler(0, 0, z);
+    } 
+}
