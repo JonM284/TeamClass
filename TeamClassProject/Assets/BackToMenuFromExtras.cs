ffdf5ad890b8b954e820e15fb7528e9437eefadf
Nolan Sadowski <nolanryansadowski@outlook.com>
2019-05-02 17:18:02 -0400

    jbsda
    asdhfbadf

---
+++ TeamClassProject/Assets/BackToMenuFromExtras.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Rewired;
+using Rewired.ControllerExtensions;
+
+public class backToMenuFromExtras : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Iterating through Players (excluding the System Player)
+        for (int i = 0; i < ReInput.players.playerCount; i++)
+        {
+            Player myPlayer = ReInput.players.Players[i];
+
+
+            if (myPlayer.GetButtonDown("HeavyAttack"))
+            {
+                SceneManager.LoadScene("NolanScene");
+            }
+        }
+    }
+}
